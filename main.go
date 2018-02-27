cf77df3e960929b4d573be7a24d51f8654576c54
David Bull <github@uk-dave.com>
2018-02-27 14:16:39 +0000

    Add initial main file

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/go-openapi/loads"
+
+	"github.com/ukdave/goswagger-todolist/gen/restapi"
+	"github.com/ukdave/goswagger-todolist/gen/restapi/operations"
+)
+
+func main() {
+	// load embedded swagger file
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// create new service API
+	api := operations.NewTodoAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+	defer server.Shutdown()
+
+	// parse flags
+	var portFlag = flag.Int("port", 3000, "The port to listen on")
+	flag.Parse()
+
+	// set the port this service will be run on
+	server.Port = *portFlag
+
+	// TODO: Set Handle
+
+	// serve API
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+}
